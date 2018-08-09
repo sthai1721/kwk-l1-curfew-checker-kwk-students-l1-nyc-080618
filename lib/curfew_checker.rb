c1a976f76f57604d5239b208fe383a024eae52c8
sthai1721 <sophia.thai@rutgers.edu>
2018-08-09 00:29:34 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -40,5 +40,14 @@ end
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  
+  if time== 11
+ return "Time to apparate!"
+elsif time>=11
+  return "You're in trouble! Better get home quick!"
+else 
+  time=11-time.to_i
+  return "You have #{time} hour(s) left to keep having fun!"
+
+end
 end
